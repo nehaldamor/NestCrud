a658adfbc36012ec052c4866dc46e8aa7dd7f876
nehaldamor <168215150+nehaldamor@users.noreply.github.com>
2026-08-06 13:50:24 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): { status: string; message: string } {
-    return { status: 'ok', message: 'Hello World!' };
+  getHello(): string {
+    return 'Hello World!';
   }
 }
