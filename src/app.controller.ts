a54f23cccca54a22de5bd0e0f0a3f78f53ca7d99
nehaldamor <168215150+nehaldamor@users.noreply.github.com>
2026-08-06 13:49:52 +0530

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello():{ status: string; message: string }{
+  getHello():string{
     return this.appService.getHello();
   }
 }
